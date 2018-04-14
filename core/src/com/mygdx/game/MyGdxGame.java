91230dc6329f8e9a07201f95f6870fef490f001f
TiagoC22 <leportuguesinho@hotmail.fr>
2018-04-15 00:54:10 +0200

    Étape d'affichage des boutons (NON-Fonctionnel)

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,31 +9,31 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.awt.*;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    SpriteBatch batch;
+    Texture img;
 
-	//Récuperation largeur/hauteur écran
-	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-	int width = gd.getDisplayMode().getWidth();
-	int height = gd.getDisplayMode().getHeight();
+    //Récuperation largeur/hauteur écran
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    int width = gd.getDisplayMode().getWidth();
+    int height = gd.getDisplayMode().getHeight();
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("wallpaper.jpg");
-	}
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        img = new Texture("wallpaper.jpg");
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render () {
 
-		batch.begin();
-		batch.draw(img, 0, 0, width, height);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        batch.begin();
+        batch.draw(img, 0, 0, width, height);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+    }
 }
