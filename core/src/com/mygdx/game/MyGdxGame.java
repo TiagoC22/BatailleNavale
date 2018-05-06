7b9c11736106df6b0352b0295b9de8c645211b7e
GautierL4 <gautier.laurent4@gmail.com>
2018-05-06 12:52:23 +0200

    Enhanced Game class

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,13 +3,29 @@ package com.mygdx.game;
 
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class MyGdxGame extends Game {
 
+    public SpriteBatch batch;
+    public BitmapFont font;
+
     @Override
     public void create () {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
         setScreen(new MainMenu(this));
+    }
+
+    public void render(){
+        super.render();
+    }
+
+    public void dispose(){
+        batch.dispose();
+        font.dispose();
     }
 
 }
